452513190ecaa3e2b8333f2c304df8048a640227
Bora Guven <guvenbe@gmail.com>
2023-04-05 21:58:19 -0400

    pageable with jackson

--- src/main/java/guru/springframework/spring6resttemplate/client/BeerClientImpl.java
+++ src/main/java/guru/springframework/spring6resttemplate/client/BeerClientImpl.java
@@ -2,6 +2,7 @@ package guru.springframework.spring6resttemplate.client;
 
 import guru.springframework.spring6resttemplate.model.BeerDTO;
 import guru.springframework.spring6resttemplate.model.BeerDTOPageImpl;
+import guru.springframework.spring6resttemplate.model.BeerStyle;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.data.domain.Page;
@@ -10,21 +11,56 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import java.net.URI;
+import java.util.UUID;
+
 @Service
 @RequiredArgsConstructor
 public class BeerClientImpl implements BeerClient {
 
     //public static final String BASE_URL = "http://localhost:8080";
     public static final String GET_BEER_PATH = "/api/v1/beer";
+    public static final String GET_BEER_BY_ID_PATH = "/api/v1/beer/{beerId}";
     private final RestTemplateBuilder restTemplateBuilder;
 
     @Override
-    public Page<BeerDTO> listBeers(String beerName) {
+    public Page<BeerDTO> listBeers() {
+        return this.listBeers(null, null, null, null , null);
+    }
+
+    @Override
+    public Page<BeerDTO> listBeers(String beerName, BeerStyle beerStyle, Boolean showInventory, Integer pageNumber,
+                                   Integer pageSize) {
         RestTemplate restTemplate = restTemplateBuilder.build();
         UriComponentsBuilder uriComponentsBuilder = UriComponentsBuilder.fromPath(GET_BEER_PATH);
         if(beerName != null){
             //This will encode any parameter
             uriComponentsBuilder.queryParam("beerName", beerName);
+        }
+
+        if(beerStyle != null){
+            //This will encode any parameter
+            uriComponentsBuilder.queryParam("beerStyle", beerStyle);
+        }
+
+        if(beerStyle != null){
+            //This will encode any parameter
+            uriComponentsBuilder.queryParam("beerStyle", beerStyle);
+        }
+
+        if(showInventory != null){
+            //This will encode any parameter
+            uriComponentsBuilder.queryParam("showInventory", showInventory);
+        }
+
+        if(pageNumber != null){
+            //This will encode any parameter
+            uriComponentsBuilder.queryParam("pageNumber", pageNumber);
+        }
+
+        if(pageSize != null){
+            //This will encode any parameter
+            uriComponentsBuilder.queryParam("pageSize", pageSize);
         }
         ResponseEntity<BeerDTOPageImpl> response =
                 restTemplate.getForEntity(uriComponentsBuilder.toUriString(), BeerDTOPageImpl.class);
@@ -47,6 +83,34 @@ public class BeerClientImpl implements BeerClient {
 //
 //        System.out.println(stringResponse.getBody());
 
+
+    }
+
+    @Override
+    public BeerDTO getBeerById(UUID beerId) {
+        RestTemplate restTemplate = restTemplateBuilder.build();
+        return restTemplate.getForObject(GET_BEER_BY_ID_PATH,BeerDTO.class, beerId);
+    }
+
+    @Override
+    public BeerDTO createBeer(BeerDTO newDto) {
+        RestTemplate restTemplate = restTemplateBuilder.build();
+        //ResponseEntity<BeerDTO> response = restTemplate.postForEntity(GET_BEER_PATH, newDto, BeerDTO.class);
+        URI uri = restTemplate.postForLocation(GET_BEER_PATH, newDto);
+        return restTemplate.getForObject(uri.getPath(), BeerDTO.class);
+    }
+
+    @Override
+    public BeerDTO updateBeer(BeerDTO beerDto) {
+        RestTemplate restTemplate = restTemplateBuilder.build();
+        restTemplate.put(GET_BEER_BY_ID_PATH,beerDto,beerDto.getId());
+        return getBeerById(beerDto.getId());
+    }
+
+    @Override
+    public void deleteBeer(UUID beerId) {
+        RestTemplate restTemplate = restTemplateBuilder.build();
+        restTemplate.delete(GET_BEER_BY_ID_PATH, beerId);
 
     }
 }
--- src/test/java/guru/springframework/spring6resttemplate/client/BeerClientImplTest.java
+++ src/test/java/guru/springframework/spring6resttemplate/client/BeerClientImplTest.java
@@ -1,22 +1,99 @@
 package guru.springframework.spring6resttemplate.client;
 
+import guru.springframework.spring6resttemplate.model.BeerDTO;
+import guru.springframework.spring6resttemplate.model.BeerStyle;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.web.client.HttpClientErrorException;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class BeerClientImplTest {
     @Autowired
     BeerClientImpl beerClient;
 
+
+    @Test
+    void testUpdateBeer() {
+        BeerDTO newDto = BeerDTO.builder()
+                .price(new BigDecimal("10.99"))
+                .beerName("Mango Bobs 2")
+                .beerStyle(BeerStyle.IPA)
+                .quantityOnHand(500)
+                .upc("123456")
+                .build();
+
+        BeerDTO beerDto = beerClient.createBeer(newDto);
+
+        final String newName = "Mango Bobs 3";
+        beerDto.setBeerName(newName);
+        BeerDTO updatedBeer = beerClient.updateBeer(beerDto);
+
+        beerClient.deleteBeer(beerDto.getId());
+        assertEquals(newName,updatedBeer.getBeerName());
+
+    }
+
+    @Test
+    void testDeleteBeer() {
+        BeerDTO newDto = BeerDTO.builder()
+                .price(new BigDecimal("10.99"))
+                .beerName("Mango Bobs 2")
+                .beerStyle(BeerStyle.IPA)
+                .quantityOnHand(500)
+                .upc("12345")
+                .build();
+
+        BeerDTO beerDTO = beerClient.createBeer(newDto);
+
+        beerClient.deleteBeer(beerDTO.getId());
+
+        assertThrows(HttpClientErrorException.class, () ->{
+            beerClient.getBeerById(beerDTO.getId());
+        });
+
+
+
+
+    }
+
+    @Test
+    void testCreateBeer() {
+        BeerDTO newDto = BeerDTO.builder()
+                .price(new BigDecimal("10.99"))
+                .beerName("Mango Bobs")
+                .beerStyle(BeerStyle.IPA)
+                .quantityOnHand(500)
+                .upc("1234555")
+                .build();
+
+        BeerDTO savedDto = beerClient.createBeer(newDto);
+        assertNotNull(savedDto);
+
+    }
+
+    @Test
+    void getBeerById() {
+        Page<BeerDTO> beerDTOS = beerClient.listBeers();
+        BeerDTO dto = beerDTOS.getContent().get(0);
+        BeerDTO byId = beerClient.getBeerById(dto.getId());
+        assertNotNull(byId);
+
+    }
+
     @Test
     void listBeersNoBeerName() {
-        beerClient.listBeers(null);
+        beerClient.listBeers(null, null,null, null, null);
 
     }
     @Test
     void listBeers() {
-        beerClient.listBeers("ALE");
+        beerClient.listBeers("ALE", null,null,null,null);
 
     }
 }
